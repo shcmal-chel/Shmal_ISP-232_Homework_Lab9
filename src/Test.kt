fbf93d6b581b6c798f722fae7334aba431a96dc0
shmal-chel <ivan_hih@icloud.com>
2026-01-13 15:48:31 +0300

    Add abstract class

--- src/Test.kt
+++ src/Test.kt
@@ -20,6 +20,11 @@ fun main() {
 
     val(id,name,quantity) = betterSword
     println("Id предмета: $id\nИмя: $name\nКоличество: $quantity\n")
+
+    val ivan: Person = Person("Ivan",18)
+    val egor: Human = Person("Egor",28)
+    ivan.hello()
+
 }
 
 data class Item(
@@ -28,5 +33,30 @@ data class Item(
     val quantity: Int) {
     override fun toString(): String {
         return "Id предмета: $id\nИмя: $name\nКоличество: $quantity\n"
+    }
+}
+
+abstract class Human(val name: String) {
+abstract var age: Int
+    abstract fun hello()
+}
+
+class Person(name:String, override var age : Int) : Human(name){
+    override fun hello(){
+        println("My name is $name")
+    }
+}
+
+abstract class Figure {
+    abstract fun perimeter(): Float
+    abstract fun area(): Float
+}
+
+class Rectangle(val width: Float, val height: Float) : Figure(){
+    override fun perimeter(): Float {
+        return 2*(width+height)
+    }
+    override fun area(): Float{
+        return width* height
     }
 }

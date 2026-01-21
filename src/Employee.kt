69527a737ea6b8c44c1f7da0edaa4fbceb76326d
Sliceeee01 <ivan_hih@icloud.com>
2026-01-21 10:41:42 +0300

    Employee class with private fields

---
+++ src/Employee.kt
@@ -0,0 +1,10 @@
+class Employee {
+    private var fullName: String = readln()
+
+    private var position: String = readln()
+
+    private var salary: Int = readln().toInt()
+
+    private var yearsOfExperience: Int = readln().toInt()
+
+}

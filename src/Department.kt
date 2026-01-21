b96c70869bf0da5d42a900d8de2f7081b6743d3f
Sliceeee01 <ivan_hih@icloud.com>
2026-01-21 11:34:42 +0300

    Implement DevelopmentDepartment and TestingDepartment

--- src/Department.kt
+++ src/Department.kt
@@ -1,2 +1,11 @@
-abstract class Department {
+abstract class Department(
+    val departmentName : String
+) {
+    fun printDepartmentGoal(){
+        println("Цель отдела: $departmentName")
+    }
 }
+abstract class DevelopmentDepartment: Department("Отдел полиции"){
+    printDepartmentGoal()
+}
+abstract class TestingDepartment: Department("Отдел Полиции")

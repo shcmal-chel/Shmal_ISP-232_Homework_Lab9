dd05bb9c84d08d5d140e496b4db86620534724e3
Sliceeee01 <ivan_hih@icloud.com>
2026-01-21 11:16:57 +0300

    Demonstrate equals, copy and toString for Task

--- src/Test.kt
+++ src/Test.kt
@@ -12,3 +12,13 @@ enum class Priority{
     MEDIUM,
     HIGH
 }
+fun main(){
+    var task_1 = Task("Купить молоко","Зайти в магазин (желательно взять по скидке)",Priority.MEDIUM,false)
+    task_1.hashCode()
+    var task_2 = Task("Купить пиву","Зайти в магазин (желательно взять пачпорт)",Priority.MEDIUM,false)
+    task_2.copy()
+    var task_3 = Task("Закончить школу","Закончить обучение в школе и уйти после 9 класса", Priority.HIGH, true)
+    task_3.toString()
+    task_3.equals(1)
+
+}

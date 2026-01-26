38bae169c0057fea8da4d73fd82696de0919a223
Ivan <shcmalivan@gmail.com>
2026-01-26 14:57:38 +0300

    ImplementDevelopmentDepartment and TestingDepartment

--- src/Department.kt
+++ src/Department.kt
@@ -1,11 +1,28 @@
-abstract class Department(
-    val departmentName : String
-) {
-    fun printDepartmentGoal(){
-        println("Цель отдела: $departmentName")
+abstract class Department {
+    abstract val departmentName: String
+    abstract fun printDepartmentGoal()
+}
+
+class DevelopmentDepartment : Department() {
+    override val departmentName = "Разработка"
+
+    override fun printDepartmentGoal() {
+        println("Цель отдела $departmentName: Писать чистый код")
     }
 }
-abstract class DevelopmentDepartment: Department("Отдел полиции"){
-    printDepartmentGoal()
+
+class TestingDepartment : Department() {
+    override val departmentName = "Тестирование"
+
+    override fun printDepartmentGoal() {
+        println("Цель отдела $departmentName: Находить все баги")
+    }
 }
-abstract class TestingDepartment: Department("Отдел Полиции")
+
+fun main() {
+    val dev = DevelopmentDepartment()
+    val test = TestingDepartment()
+
+    dev.printDepartmentGoal()
+    test.printDepartmentGoal()
+}
